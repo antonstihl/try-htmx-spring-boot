35e0ae6ae108644fbf0911ad8e02eaf77aab82f9
Anton Stihl <anton.stihl@nordnet.se>
2024-03-15 22:32:38 +0100

    enhanced apple functionality

--- src/main/java/dev/antonstihl/tryhtmxspringboot/TryHtmxApplication.java
+++ src/main/java/dev/antonstihl/tryhtmxspringboot/TryHtmxApplication.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -17,33 +19,57 @@ public class TryHtmxApplication {
     }
 
     @Controller
-    public class AppleController {
+    public static class AppleController {
         private final AppleService appleService;
 
         public AppleController(AppleService appleService) {
             this.appleService = appleService;
         }
 
-        @GetMapping("/apple")
+        @GetMapping("/random-apple")
         public String apple(Model model) {
             model.addAttribute("applename", appleService.getRandomAppleName());
+            return "apple";
+        }
+
+        @GetMapping("/apples")
+        public String apples(Model model) {
+            model.addAttribute("apples", appleService.getApples());
             return "apples";
+        }
+
+        @PostMapping("/apples")
+        public void postApples(AppleForm appleForm) {
+            appleService.addApple(appleForm.name());
         }
     }
 
+    public record AppleForm(String name) {
+    }
+
     @Service
-    public class AppleService {
+    public static class AppleService {
+
+        private List<String> apples = new ArrayList<>(List.of(
+                "Cox's Orange Pippin",
+                "Braeburn",
+                "Granny Smith",
+                "Golden Delicious",
+                "Pink Lady",
+                "Fuji",
+                "Ingrid-Marie"
+        ));
+
         public String getRandomAppleName() {
-            var Apples = List.of(
-                    "Cox's Orange Pippin",
-                    "Braeburn",
-                    "Granny Smith",
-                    "Golden Delicious",
-                    "Pink Lady",
-                    "Fuji",
-                    "Ingrid-Marie"
-            );
-            return Apples.get((int) (Math.random() * Apples.size()));
+            return apples.get((int) (Math.random() * apples.size()));
+        }
+
+        public List<String> getApples() {
+            return apples;
+        }
+
+        public void addApple(String name) {
+            apples.add(name);
         }
     }
 }
