cbe4dd71c3eb8bd6049c767f5697c1de8a3a3eed
Anton Stihl <anton.stihl@nordnet.se>
2024-03-16 08:23:25 +0100

    more mutations + poll apples

--- src/main/java/dev/antonstihl/tryhtmxspringboot/TryHtmxApplication.java
+++ src/main/java/dev/antonstihl/tryhtmxspringboot/TryHtmxApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @SpringBootApplication
 public class TryHtmxApplication {
@@ -42,34 +45,46 @@ public class TryHtmxApplication {
         public void postApples(AppleForm appleForm) {
             appleService.addApple(appleForm.name());
         }
+
+        @DeleteMapping("/apples/{id}")
+        public void deleteApple(@PathVariable UUID id) {
+            appleService.deleteApple(id);
+        }
     }
 
     public record AppleForm(String name) {
     }
 
+    public record Apple(UUID id, String name) {
+    }
+
     @Service
     public static class AppleService {
 
-        private List<String> apples = new ArrayList<>(List.of(
-                "Cox's Orange Pippin",
-                "Braeburn",
-                "Granny Smith",
-                "Golden Delicious",
-                "Pink Lady",
-                "Fuji",
-                "Ingrid-Marie"
+        private final List<Apple> apples = new ArrayList<>(List.of(
+                new Apple(UUID.randomUUID(), "Cox's Orange Pippin"),
+                new Apple(UUID.randomUUID(), "Braeburn"),
+                new Apple(UUID.randomUUID(), "Granny Smith"),
+                new Apple(UUID.randomUUID(), "Golden Delicious"),
+                new Apple(UUID.randomUUID(), "Pink Lady"),
+                new Apple(UUID.randomUUID(), "Fuji"),
+                new Apple(UUID.randomUUID(), "Ingrid-Marie")
         ));
 
-        public String getRandomAppleName() {
+        public Apple getRandomAppleName() {
             return apples.get((int) (Math.random() * apples.size()));
         }
 
-        public List<String> getApples() {
+        public List<Apple> getApples() {
             return apples;
         }
 
         public void addApple(String name) {
-            apples.add(name);
+            apples.add(new Apple(UUID.randomUUID(), name));
+        }
+
+        public void deleteApple(UUID id) {
+            apples.removeIf(a -> a.id.equals(id));
         }
     }
 }
